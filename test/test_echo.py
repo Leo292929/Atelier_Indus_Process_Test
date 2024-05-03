b7bb2e0389deafe280ad1ab8d38d1821d22d9cb3
Leo292929 <leo.fougeroud@ecoles-epsi.net>
2024-05-03 13:22:42 +0200

    [RED] test avec random word

--- test/test_echo.py
+++ test/test_echo.py
@@ -1,15 +1,21 @@
 import unittest
 from src.verificateur_palindrome import VerificateurPalindrome
-
+import random
+import string
 
 
 class Palindrome(unittest.TestCase):
+
+    def motaleatoire(cls, length):
+        lettres = string.ascii_lowercase
+        return ''.join(random.choice(lettres) for i in range(length))
+
 
     def test_palindrome(self):
         
     # ETANT DONNE 1 liste de mots
 
-        cas  = ["epsi","test"]
+        cas  = ["epsi","test","palindrome",self.motaleatoire(10),self.motaleatoire(100)]
 
     # QUAND on verifie si se sont des palindrome
         
