1f47f8b4b1d9840c88b64617af19ad52c23c2492
Leo292929 <leo.fougeroud@ecoles-epsi.net>
2024-05-03 14:11:22 +0200

    [RED] correction commit precedent

--- test/test_echo.py
+++ test/test_echo.py
@@ -19,22 +19,15 @@ class Palindrome(unittest.TestCase):
     def test_pas_palindrome(self):
         
     # ETANT DONNE 1 liste de mots
-
         cas  = ["epsi","test","palindrome",self.motaleatoire(10),self.motaleatoire(100)]
-
     # QUAND on verifie si se sont des palindrome
-        
-
         for chaine in cas:
             with self.subTest(chaine):
                 resultat = VerificateurPalindrome.verif(chaine)
-
-
     # ALORS la chaîne est renvoyée en miroir
-
                 attendu = chaine[::-1]
-
                 self.assertEqual(attendu, resultat)
+
 
     def test_est_palindrome(self):
 
@@ -47,6 +40,18 @@ class Palindrome(unittest.TestCase):
     #ALORS celui-ci est renvoyé #ET « Bien dit » est envoyé ensuite
                 self.assertEqual(chaine + " Bien dit", resultat)
     
+
+    def test_bonjour_aurevoir(self):
+
+    # ETANT DONNE une chaine de caractere
+        chaine = "toi"
+    #QUAND on la saisie
+        resultat = VerificateurPalindrome.verif(chaine)
+    #ALORS « Bonjour » est envoyé avant toute réponse
+        #self.assertEqual("Bonjour ", resultat.split(" ")[0])
+        self.assertEqual("Bonjour toi", resultat)
+
+
 
 if __name__ == '__main__':
     unittest.main()
