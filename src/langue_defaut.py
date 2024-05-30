20f22186814aaa15f5e3f1f1afb7b4c982601db4
Leo292929 <leo.fougeroud@ecoles-epsi.net>
2024-05-30 17:13:12 +0200

    [GREEN] avec modification de la langue defaut

--- src/langue_defaut.py
+++ src/langue_defaut.py
@@ -1,12 +1,22 @@
+from moment import Moment
+
+
 class langueDefaut:
     BIEN_DIT = " x x"
-    BONJOUR = "x "
-    AU_REVOIR = " x x"
+    BONJOUR_AM = "x x "
+    AU_REVOIR_AM = " x x x\r\n"
+    BONJOUR_PM = "x x "
+    AU_REVOIR_PM = " x x x\r\n"
+
 
     @classmethod
     def feliciter(cls):
         return cls.BIEN_DIT
 
     @classmethod
-    def saluer(cls):
-        return [cls.BONJOUR, cls.AU_REVOIR]
+    def saluer(cls, moment):
+        if moment == 0:
+            return [cls.BONJOUR_AM, cls.AU_REVOIR_AM]
+        else:
+            return [cls.BONJOUR_PM, cls.AU_REVOIR_PM]
+
